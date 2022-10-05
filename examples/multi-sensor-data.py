63ed4407ced771f98f6c992156a3c60a5e0f4fbd
ColbySawyer7 <sawyerc17@students.ecu.edu>
2022-10-05 10:52:38 -0400

    Update multi-sensor-data.py

--- examples/multi-sensor-data.py
+++ examples/multi-sensor-data.py
@@ -3,13 +3,8 @@
 # It is recommended that you prepackage your data in a bytearray here so that you can have have a sketch for the decoder later
 import psutil
 from func_timeout import func_timeout, FunctionTimedOut
-from devices.hydros import Hydros
-from devices.opc import OPC
-from devices.sen5x import Sen5x
+from esdn_sensing import Hydros, OPC, SEN5x, SensorError
 
-
-# Or use the Library (Once its available)
-# import ecu-sensing
 
 def use_hydros():
     return Hydros().get_data()
@@ -18,7 +13,7 @@ def use_opc():
     return OPC().get_data()
 
 def use_sen5x():
-    return Sen5x().get_data()
+    return SEN5x().get_data()
 
 def get_data():
     # Here is where you can add your sensor specific information. The driving code will use whatever data is here
@@ -43,9 +38,10 @@ def get_data():
     try:
         sensor1_data = func_timeout(30, use_hydros)
         sensor1_state = True
-    except FunctionTimedOut:
+    except (FunctionTimedOut, SensorError) as err:
         exception = 11
         error_state_count += 1
+        print(err)
     except Exception as err:
         exception = 1
         print(err)
@@ -54,9 +50,10 @@ def get_data():
     try:
         sensor2_data = func_timeout(30, use_opc)
         sensor2_state = True
-    except FunctionTimedOut:
+    except (FunctionTimedOut, SensorError) as err:
         exception = 12
         error_state_count += 1
+        print(err)
     except Exception as err:
         exception = 1
         print(err)
@@ -65,14 +62,16 @@ def get_data():
     try:
         sensor3_data = func_timeout(30, use_sen5x)
         sensor3_state = True
-    except FunctionTimedOut:
+    except (FunctionTimedOut, SensorError) as err:
         exception = 13
         error_state_count += 1
+        print(err)
     except Exception as err:
         exception = 1
         print(err)
 
-    if error_state_count > 2:
+    # If more than one device error state default to exception code 10
+    if error_state_count > 1:
         exception = 10
 
     # Exception to Start
@@ -80,13 +79,13 @@ def get_data():
     sensor_data[1] = exception & 0xff
     # Sensor 1
     if sensor1_state:
-        sensor_data.append(sensor1_data)
+        sensor_data.extend(sensor1_data)
     # Sensor 2
     if sensor2_state:
-        sensor_data.append(sensor2_data)
+        sensor_data.extend(sensor2_data)
     # Sensor 3
     if sensor3_state:
-        sensor_data.append(sensor3_data)
+        sensor_data.extend(sensor3_data)
     # CPU Stats
     cpu_val = int((psutil.cpu_percent(4) * 100))
     print('The CPU usage is: ', cpu_val)
